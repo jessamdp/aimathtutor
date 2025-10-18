b40e2d5e05aa3c7b5a7e8f90605191fd99e2d0b2
Gregor Dietrich <11874515+gregor-dietrich@users.noreply.github.com>
2025-10-18 02:19:55 +0200

    Use buildx for multi-platform Docker image builds in build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ mvn clean install -DskipTests
 mvn test
 mvn package -DskipTests -Pproduction
 
-docker build -t gregordietrich/aimathtutor:1.0.0-SNAPSHOT -f src/main/docker/Dockerfile.jvm .
+docker buildx build --platform linux/amd64,linux/arm64 -t gregordietrich/aimathtutor:1.0.0-SNAPSHOT -f src/main/docker/Dockerfile.jvm .
