5de05f915744885e66190a7cba6e5aef5bb0c495
Gregor Dietrich <11874515+gregor-dietrich@users.noreply.github.com>
2025-10-18 19:23:53 +0200

    Add ReportButton component with confirmation dialog for reporting inappropriate content

---
+++ src/main/java/de/vptr/aimathtutor/component/button/ReportButton.java
@@ -0,0 +1,38 @@
+package de.vptr.aimathtutor.component.button;
+
+import org.vaadin.lineawesome.LineAwesomeIcon;
+
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+
+import de.vptr.aimathtutor.component.dialog.ConfirmationDialog;
+
+public class ReportButton extends Button {
+    private final static String DEFAULT_TOOLTIP = "Report as inappropriate";
+
+    private final ComponentEventListener<ClickEvent<Button>> reportAction;
+
+    public ReportButton(final ComponentEventListener<ClickEvent<Button>> reportAction, final String tooltipText) {
+        super("");
+        this.reportAction = reportAction;
+        this.addThemeVariants(ButtonVariant.LUMO_SMALL, ButtonVariant.LUMO_ERROR);
+        this.setIcon(LineAwesomeIcon.FLAG_SOLID.create());
+        this.setTooltipText(tooltipText != null ? tooltipText : DEFAULT_TOOLTIP);
+        this.addClickListener(this::showConfirmationDialog);
+    }
+
+    public ReportButton(final ComponentEventListener<ClickEvent<Button>> reportAction) {
+        this(reportAction, null);
+    }
+
+    private void showConfirmationDialog(final ClickEvent<Button> event) {
+        final var confirmDialog = new ConfirmationDialog(e -> {
+            if (this.reportAction != null) {
+                this.reportAction.onComponentEvent(event);
+            }
+        });
+        confirmDialog.open();
+    }
+}
