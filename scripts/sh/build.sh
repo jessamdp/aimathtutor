5f44548f362d9d91574b0ab087f5c011e13c1eb8
Gregor Dietrich <11874515+gregor-dietrich@users.noreply.github.com>
2025-12-06 15:37:59 +0100

    Refactor build script to support separate Dockerfiles for Alpine and UBI images

--- scripts/sh/build.sh
+++ scripts/sh/build.sh
@@ -4,7 +4,8 @@
 . "$DIR/lib/get_maven.sh"
 
 TAG="gregordietrich/aimathtutor:2.1.0"
-DOCKERFILE="src/main/docker/Dockerfile.alpine"
+DOCKERFILE_ALPINE="src/main/docker/Dockerfile.alpine"
+DOCKERFILE_UBI="src/main/docker/Dockerfile.ubi"
 PLATFORMS="linux/amd64,linux/arm64"
 
 prompt_yes_no() {
@@ -53,19 +54,36 @@ ${MVN_CMD} package -DskipTests -Pproduction
 # This avoids starting a docker-container builder that may reference Docker Desktop/WSL bind mounts
 if docker buildx inspect default >/dev/null 2>&1; then
 	echo "Using buildx 'default' builder to build image. If you want to push multi-arch images, add --push."
-	if docker buildx build --builder default --platform "$PLATFORMS" -t "$TAG" -f "$DOCKERFILE" .; then
+
+    # Alpine-based image
+	if docker buildx build --builder default --platform "$PLATFORMS" -t "$TAG"-alpine -f "$DOCKERFILE_ALPINE" .; then
 		echo "buildx multi-platform build finished (results kept in buildx cache). Use --push to publish or --load for a single-platform image."
 	else
 		echo "buildx multi-platform build failed; attempting single-platform local build with --load for current arch."
 		# Try loading a single-platform image into local docker (amd64). If that fails, fallback to plain docker build.
-		if ! docker buildx build --load --platform linux/amd64 -t "$TAG" -f "$DOCKERFILE" .; then
+		if ! docker buildx build --load --platform linux/amd64 -t "$TAG"-alpine -f "$DOCKERFILE_ALPINE" .; then
 			echo "--load build failed; falling back to plain 'docker build' (single-platform)."
-			docker build -t "$TAG" -f "$DOCKERFILE" .
+			docker build -t "$TAG"-alpine -f "$DOCKERFILE_ALPINE" .
+		fi
+	fi
+
+    # UBI-based image
+    if docker buildx build --builder default --platform "$PLATFORMS" -t "$TAG"-ubi -f "$DOCKERFILE_UBI" .; then
+		echo "buildx multi-platform build finished (results kept in buildx cache). Use --push to publish or --load for a single-platform image."
+	else
+		echo "buildx multi-platform build failed; attempting single-platform local build with --load for current arch."
+		# Try loading a single-platform image into local docker (amd64). If that fails, fallback to plain docker build.
+		if ! docker buildx build --load --platform linux/amd64 -t "$TAG"-ubi -f "$DOCKERFILE_UBI" .; then
+			echo "--load build failed; falling back to plain 'docker build' (single-platform)."
+			docker build -t "$TAG"-ubi -f "$DOCKERFILE_UBI" .
 		fi
 	fi
 else
 	echo "buildx 'default' builder not available; performing plain docker build (single-platform)."
-	docker build -t "$TAG" -f "$DOCKERFILE" .
+	docker build -t "$TAG"-alpine -f "$DOCKERFILE_ALPINE" .
+	docker build -t "$TAG"-ubi -f "$DOCKERFILE_UBI" .
 fi
+
+docker tag "$TAG"-ubi "$TAG"
 
 cd - > /dev/null
